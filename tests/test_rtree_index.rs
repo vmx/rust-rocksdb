519cb812fab00c7c1d4f182716c567d1d1a178d1
Volker Mische <volker.mische@gmail.com>
2017-09-26 16:18:34 +0200

    Adapt tests to most recent version

--- tests/test_rtree_index.rs
+++ tests/test_rtree_index.rs
@@ -17,6 +17,7 @@ extern crate libc;
 extern crate librocksdb_sys as ffi;
 extern crate rocksdb;
 
+use std::cmp::Ordering;
 use std::{slice, str};
 use std::mem;
 
@@ -24,6 +25,15 @@ use self::integer_encoding::VarInt;
 
 use rocksdb::{BlockBasedOptions, BlockBasedIndexType, DB, DBIterator, Options};
 
+
+/// Return the slice that is prefixed with an unsigned 32-bit varint and the offset after
+/// the slice that was read
+fn get_length_prefixed_slice(data: &[u8]) -> (&[u8], usize) {
+    let (size, slice_start) = u32::decode_var(data);
+    let slice_end = slice_start + size as usize;
+    let slice = &data[slice_start..slice_end];
+    (slice, slice_end)
+}
 
 fn values_from_iter(iter: DBIterator) -> Vec<String>{
     let mut vec = Vec::new();
@@ -34,13 +44,12 @@ fn values_from_iter(iter: DBIterator) -> Vec<String>{
     vec
 }
 
-fn serialize_key(keypath: &str, iid: f64 , aa_min: f64, aa_max: f64, bb_min: f64, bb_max: f64) -> Vec<u8> {
+fn serialize_key(keypath: &str, iid: u64 , aa_min: f64, aa_max: f64, bb_min: f64, bb_max: f64) -> Vec<u8> {
     let mut key = Vec::new();
     key.append(&mut keypath.len().encode_var_vec());
     key.extend_from_slice(keypath.as_bytes());
     // The R-tree stores boxes, hence duplicate the input values
-    key.extend_from_slice(&unsafe{ mem::transmute::<f64, [u8; 8]>(iid) });
-    key.extend_from_slice(&unsafe{ mem::transmute::<f64, [u8; 8]>(iid) });
+    key.extend_from_slice(&unsafe{ mem::transmute::<u64, [u8; 8]>(iid) });
     key.extend_from_slice(&unsafe{ mem::transmute::<f64, [u8; 8]>(aa_min) });
     key.extend_from_slice(&unsafe{ mem::transmute::<f64, [u8; 8]>(aa_max) });
     key.extend_from_slice(&unsafe{ mem::transmute::<f64, [u8; 8]>(bb_min) });
@@ -49,13 +58,13 @@ fn serialize_key(keypath: &str, iid: f64 , aa_min: f64, aa_max: f64, bb_min: f64
     return key;
 }
 
-fn serialize_query(keypath: &str, iid_min: f64, iid_max: f64, aa_min: f64, aa_max: f64, bb_min: f64, bb_max: f64)
+fn serialize_query(keypath: &str, iid_min: u64, iid_max: u64, aa_min: f64, aa_max: f64, bb_min: f64, bb_max: f64)
     -> Vec<u8> {
         let mut key = Vec::new();
         key.append(&mut keypath.len().encode_var_vec());
         key.extend_from_slice(keypath.as_bytes());
-        key.extend_from_slice(&unsafe{ mem::transmute::<f64, [u8; 8]>(iid_min) });
-        key.extend_from_slice(&unsafe{ mem::transmute::<f64, [u8; 8]>(iid_max) });
+        key.extend_from_slice(&unsafe{ mem::transmute::<u64, [u8; 8]>(iid_min) });
+        key.extend_from_slice(&unsafe{ mem::transmute::<u64, [u8; 8]>(iid_max) });
         key.extend_from_slice(&unsafe{ mem::transmute::<f64, [u8; 8]>(aa_min) });
         key.extend_from_slice(&unsafe{ mem::transmute::<f64, [u8; 8]>(aa_max) });
         key.extend_from_slice(&unsafe{ mem::transmute::<f64, [u8; 8]>(bb_min) });
@@ -63,31 +72,58 @@ fn serialize_query(keypath: &str, iid_min: f64, iid_max: f64, aa_min: f64, aa_ma
         return key;
 }
 
-/*
-fn deserialize_key(key: Vec<u8>) -> (String, Vec<f64>) {
-    let mut key_slice = key.as_slice();
-
-    // Get keypath
-    let (size, num_bytes) = u32::decode_var(key_slice);
-    let size = size as usize;
-    key_slice = &key_slice[num_bytes..];
-    let keypath = str::from_utf8(&key_slice[0..size]).unwrap().to_string();
-    key_slice = &key_slice[size..];
-
-    // Get other dimensions
-    let mut mbb = Vec::new();
-    while key_slice.len() > 0 {
-        let mut tmp: [u8; 8] = [0; 8];
-        tmp.copy_from_slice(&key_slice[0..8]);
-        let double = unsafe { mem::transmute::<[u8; 8], f64>(tmp) };
-        mbb.push(double);
-        key_slice = &key_slice[8..];
+fn compare_keys_rtree(aa: &[u8], bb: &[u8]) -> Ordering {
+    if aa.len() == 0 && bb.len() == 0{
+        return Ordering::Equal;
+    } else if aa.len() == 0 {
+        return Ordering::Less;
+    } else if bb.len() == 0 {
+        return Ordering::Greater;
     }
 
-    (keypath, mbb)
-}
-*/
+    let (keypath_aa, offset_aa) = get_length_prefixed_slice(aa);
+    let (keypath_bb, offset_bb) = get_length_prefixed_slice(bb);
 
+    // The ordering of the keypath doesn't need to be unicode collated. The ordering
+    // doesn't really matters, it only matters that it's always the same.
+    let keypath_compare = keypath_aa.cmp(keypath_bb);
+    if keypath_compare != Ordering::Equal {
+        return keypath_compare;
+    }
+
+    // Keypaths are the same, compare the Internal Ids value
+    let seq_aa = unsafe {
+        let array = *(aa[(offset_aa)..].as_ptr() as *const [_; 8]);
+        mem::transmute::<[u8; 8], u64>(array)
+    };
+    let seq_bb = unsafe {
+        let array = *(bb[(offset_bb)..].as_ptr() as *const [_; 8]);
+        mem::transmute::<[u8; 8], u64>(array)
+    };
+    let seq_compare = seq_aa.cmp(&seq_bb);
+    if seq_compare != Ordering::Equal {
+        return seq_compare;
+    }
+
+    // Internal Ids are the same, compare the bounding box
+    let bbox_aa = unsafe {
+        let array = *(aa[(offset_aa + 8)..].as_ptr() as *const [_; 32]);
+        mem::transmute::<[u8; 32], [f64; 4]>(array)
+    };
+    let bbox_bb = unsafe {
+        let array = *(bb[(offset_bb + 8)..].as_ptr() as *const [_; 32]);
+        mem::transmute::<[u8; 32], [f64; 4]>(array)
+    };
+
+    for (value_aa, value_bb) in bbox_aa.into_iter().zip(bbox_bb.into_iter()) {
+        let value_compare = value_aa.partial_cmp(value_bb).unwrap();
+        if value_compare != Ordering::Equal {
+            return value_compare;
+        }
+    }
+    // No early return, the values are fully equal
+    return Ordering::Equal;
+}
 
 #[test]
 pub fn test_rtree_index() {
@@ -103,6 +139,7 @@ pub fn test_rtree_index() {
     let mut block_based_opts = BlockBasedOptions::default();
     block_based_opts.set_index_type(BlockBasedIndexType::RtreeSearch);
     rtree_opts.set_block_based_table_factory(&block_based_opts);
+    rtree_opts.set_comparator("noise_rtree_cmp", compare_keys_rtree);
 
     // Start a new scope, else the database can't be destroyed at the end of the test
     {
@@ -110,40 +147,40 @@ pub fn test_rtree_index() {
         let rtree = db.create_cf("rtree", &rtree_opts).unwrap();
 
         let keypath = "somekeypath";
-        let augsburg_key = serialize_key(keypath, 10f64, 10.75, 11.11, 48.24, 48.50);
+        let augsburg_key = serialize_key(keypath, 10, 10.75, 11.11, 48.24, 48.50);
         let augsburg_key_slice = unsafe {
             slice::from_raw_parts(augsburg_key.as_ptr() as *const u8, augsburg_key.len())
         };
         db.put_cf(rtree, augsburg_key_slice, b"augsburg").unwrap();
-        let alameda_key = serialize_key(keypath, 25f64, -122.34, -122.22, 37.71, 37.80);
+        let alameda_key = serialize_key(keypath, 25, -122.34, -122.22, 37.71, 37.80);
         let alameda_key_slice = unsafe {
             slice::from_raw_parts(alameda_key.as_ptr() as *const u8, alameda_key.len())
         };
         db.put_cf(rtree, alameda_key_slice, b"alameda").unwrap();
 
         {
-            let query = serialize_query(keypath, 2f64, 15f64, 10.0, 11.0, 48.0, 49.0);
+            let query = serialize_query(keypath, 2, 15, 10.0, 11.0, 48.0, 49.0);
             let iter = db.rtree_iterator(&query.as_slice());
             let result = values_from_iter(iter);
             assert_eq!(vec!["augsburg".to_string()], result);
         }
 
         {
-            let query = serialize_query(keypath, 2f64, 50f64, -150.0, 0.0, 20.0, 40.0);
+            let query = serialize_query(keypath, 2, 50, -150.0, 0.0, 20.0, 40.0);
             let iter = db.rtree_iterator(&query.as_slice());
             let result = values_from_iter(iter);
             assert_eq!(vec!["alameda".to_string()], result);
         }
 
         {
-            let query = serialize_query(keypath, 2f64, 3f64, 10.0, 11.0, 0.0, 0.1);
+            let query = serialize_query(keypath, 2, 3, 10.0, 11.0, 0.0, 0.1);
             let iter = db.rtree_iterator(&query.as_slice());
             let result = values_from_iter(iter);
             assert!(result.is_empty());
         }
 
         {
-            let query = serialize_query(keypath, 2f64, 60f64, -180.0, 180.0, -90.0, 90.0);
+            let query = serialize_query(keypath, 2, 60, -180.0, 180.0, -90.0, 90.0);
             let iter = db.rtree_iterator(&query.as_slice());
             let result = values_from_iter(iter);
             assert_eq!(vec!["augsburg".to_string(), "alameda".to_string()],
